b483db33d08873d1ec18596f2fcf70cf61ce0af9
Sharath <sharathnalla026@gmail.com>
2026-03-01 16:08:12 +0530

    done

---
+++ All js/Operator/Assignment.js
@@ -0,0 +1,54 @@
+/*
+
+   Assignment Operator
+
+   1.=
+   2.+=
+   3.-=
+   4.*=
+   5./=
+   6.%=
+   7.**=
+
+*/
+
+
+let a = 10 ;
+
+a+=5;
+
+let b = 20 ;
+
+b-=5
+
+console.log(a);
+
+console.log(b);
+
+let c = 10;
+
+c*=2
+
+console.log(c);
+
+let d = 20;
+
+d/=10;
+
+
+
+console.log(d);
+
+
+let e = 10;
+
+e%=3;
+
+console.log(e);
+
+let f =20;
+
+f**=2;    //20^2=400
+
+
+console.log(f);
