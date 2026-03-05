fabf545e65ff2471f9b2a598a7a54a6dd4cef190
Sharath <sharathnalla026@gmail.com>
2026-03-05 16:54:22 +0530

    done

---
+++ All js/Array.js/forth.js
@@ -0,0 +1,9 @@
+const name = ["sharath","nalla", 25];
+// console.log(name.toString());
+
+const names = ["alice","bob","happy"];
+
+// console.log(names.join("-"));
+
+
+console.log(names.indexOf("bob"));
