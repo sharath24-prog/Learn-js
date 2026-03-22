09fcbde141153d83a267c7976f21e469377d85e5
Sharath <sharathnalla026@gmail.com>
2026-03-22 19:23:40 +0530

    done

--- All js/for.js/foreach/filter/index.js
+++ All js/for.js/foreach/filter/index.js
@@ -13,11 +13,21 @@
     // console.log(newArr);
 
 
-     const arr = [10,20,30,4,5];
+    //  const arr = [10,20,30,4,5];
 
-     const nw = arr.filter((number)=>number>0);
+    //  const nw = arr.filter((number)=>number>0);
 
-     console.log(nw);
+    //  console.log(nw);
+
+    
+
+    //  const arr_1 = [10,20,30,40,45];
+
+    //  const wep = arr_1.filter((number)=>number>1);
+
+    //  console.log(wep);
+
+
 
     
     
---
+++ All js/for.js/foreach/filter/prototype.js
@@ -0,0 +1,13 @@
+// const arr= [10,20,30,40,50];
+// const nw =  arr.map((num)=>num*3);
+// console.log(nw);  
+
+
+// double and triple kar deta num*3.
+
+
+
+const products = [
+
+    {id:1,name:Laptop,category:"Electronix"}
+]
